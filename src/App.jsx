1d2e6aaacd909868886ab7284ba2fb48936c61b5
Samuele <samuuuderiu@gmail.com>
2025-06-09 16:24:10 +0200

    Vero inizio progetto app To do
    Da questo commit in poi inizia il vero progetto

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+import './Index.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>CIAO</h1>
+      <h1 className='bg-red-500'>CIAO</h1>
       <strong>Samuele</strong>
     </>
   )
